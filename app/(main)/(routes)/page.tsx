d6b30e389ef2de2051dd7baa34f36a09adbb06d8
codeonoto <onoto0409@gmail.com>
2024-03-05 00:22:09 +0530

    Toggle DarkMode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle';
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <div>This is a Protected Route.</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
+    </div>
+  );
 }
